fc8ff98fd52e60db05a35f2d1672f2bd829d05f2
Ben Wakefield <benwakefield@Mac.Home>
2025-09-04 16:56:56 +0100

    Save of python test code

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+
+x = np.pi * np.arctan(0.5)
+print(x)
+
+if x < 10000:
+    print('Hello world')
+
