555b6e48b289c7694b9d675c022f3cded90f0a57
Ben Wakefield <wakefield.ben@outlook.com>
2025-09-04 17:31:18 +0100

    testing the dual author committer thing

---
+++ testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  4 17:30:16 2025
+
+@author: benwakefield
+"""
+
+#another python file, trying to fix the author-committer thingy
+
+print('hello world')
