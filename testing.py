5e98ccd9b371f88085221163962f192397307639
Ben Wakefield <wakefield.ben@outlook.com>
2025-09-05 11:24:10 +0100

    new change to python test file

--- testing.py
+++ testing.py
@@ -9,3 +9,9 @@ Created on Thu Sep  4 17:30:16 2025
 #another python file, trying to fix the author-committer thingy
 
 print('hello world')
+i = [1, 10, 10]
+j = list(range(1, 11))
+
+print(j)
+
+print(i)
